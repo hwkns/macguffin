c5b36de8a222157fa919c4408b71a31e206bb865
Daniel Hawkins <daniel@unlikelabs.org>
2013-11-21 02:32:27 -0500

    - combined Scene and P2P release group sets into a single file

--- metadata/__init__.py
+++ metadata/__init__.py
@@ -4,5 +4,4 @@ from .utils import *
 from .imdb import IMDb, IMDbError
 from .tmdb import TMDB, TMDBError
 from .mediainfo import Mediainfo, MediainfoError
-from .release_groups_p2p import p2p_groups
-from .release_groups_scene import scene_groups
+from .release_groups import p2p_groups, scene_groups
