582c2b5eb0094531df01e16323b88c911cc62475
supergonkas <matigonkas@outlook.com>
2016-06-10 22:44:08 +0100

    Use logger instead of print to avoid unicode issues

--- uploads/upload.py
+++ uploads/upload.py
@@ -331,7 +331,7 @@ class Upload(object):
             msg = 'Release title "{r}" does not match IMDb title "{i}".'
             logging.warning(msg.format(r=self.release.title, i=self.imdb.title))
             prompt = '\nIs this the correct IMDb link for the release? (Y/N)  {link}'
-            print(prompt.format(link=self.imdb))
+            logging.info(prompt.format(link=self.imdb))
             answer = raw_input()
             print()
             if answer.lower() == 'y':
